f554f37bc5e4e63714d0e222944d6308bb7aea47
CristelRR <cristel7080@gmail.com>
2025-12-01 01:32:03 -0600

    para npm run deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// AQUI DEBE IR el nombre EXACTO de tu repo
 export default defineConfig({
   plugins: [react()],
+  base: '/petcare-download/',
 })
